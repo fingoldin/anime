5d52194d913523e558e1c0d7d9a7fcbabe687010
Rajat Mittal <rajatmittal@college.harvard.edu>
2020-05-05 01:25:58 -0500

    redesigned kernel, working

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-users = pd.read_csv("clean_data/users.csv")
+users = pd.read_csv("clean_data/users.csv") # each row is a user, each column is an anime (each cell contains the users score of that anime)
+users_array = users.to_numpy()
+
 
 all_anime = pd.read_csv("data/AnimeList.csv")
 
@@ -13,37 +16,58 @@ for idx in range(len(anime_id_map_reverse)):
 
 animes = []
 test_users = [ [
-  { "anime_id": anime_id_map_reverse[3], "score": 8.0 },
-] ]
+  { "anime_id": 16498, "score": 1.0 }, # Attack on Titan
+  { "anime_id": 30276, "score": 1.0 } # One Punch Man
+],
+  [{ "anime_id": 16498, "score": 10.0 }], # Attack on Titan
+  [{ "anime_id": 30276, "score": 7.0 }] # One Punch Man
+
+]
 
 for anime_id in range(len(users.columns)):
   animes.append(users[users[str(anime_id)] != 0.0])
 
-def kernel(anime_users, test_user):
-  val = anime_users.dot(test_user).mean()
-  return val
+def kernel(user1, user2):
+  sim = 0.0
+  dif = np.zeros(user1.shape)
+  for i in range(len(user1)):
+    if (user1[i] == 0 or user2[i] == 0):
+        dif[i] = 0.0
+    else:
+        dif[i] = user1[i] - user2[i]
+        sim += 1.0
+  norm = math.sqrt(np.sum(dif * dif))/sim if sim != 0 else 1000
+  return math.exp(-norm)
 
 def convert_test(test_user):
   test_vector = np.zeros(len(animes))
   for anime in test_user:
-    test_vector[anime_id_map[anime["anime_id"]]] = anime["score"] - 5
+    test_vector[anime_id_map[anime["anime_id"]]] = anime["score"]
 
   return test_vector
 
 def predict(test_user):
   best_score = float("-inf")
   best_anime = -1
-  for anime_id in range(len(animes)):
-    score = kernel(animes[anime_id], test_user)
-    print(anime_id, ": ", score, " ", animes[anime_id][str(anime_id)])
-    if score > best_score and test_user[anime_id] == 0.0:
-      best_anime = anime_id
-      best_score = score
+
+  anime = np.zeros(len(test_user))
+
+  for user in users_array:
+      s = kernel(test_user, user)
+      anime = anime + s * user
+
+  for i in range(len(anime)):
+      if (anime[i] > best_score and test_user[i] == 0.0):
+          best_anime = i
+          best_score = anime[i]
 
   return best_anime
 
 def convert_predict(anime_id):
   return all_anime[all_anime["anime_id"] == anime_id_map_reverse[anime_id]].iloc[0]["title"]
 
+# for test_user in test_users:
+#  print("Predicting for [", ",".join([ all_anime[all_anime["anime_id"] == anime["anime_id"]].iloc[0]["title"] for anime in test_user ]), "]:  ", convert_predict(predict(convert_test(test_user))))
 for test_user in test_users:
-  print("Predicting for [", ",".join([ all_anime[all_anime["anime_id"] == anime["anime_id"]].iloc[0]["title"] for anime in test_user ]), "]:  ", convert_predict(predict(convert_test(test_user))))
+    converted = convert_test(test_user)
+    print(convert_predict(predict(converted)))
--- user_data.py
+++ user_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 
-rel_data = pd.read_csv("data/UserAnimeList.csv", nrows=200)
+rel_data = pd.read_csv("data/UserAnimeList.csv", nrows=100000)
 
 rel_data = rel_data[rel_data["my_score"] != 0.0]
 
@@ -18,8 +18,8 @@ for idx in range(len(unique_anime)):
 for user in unique_users:
   user_anime = rel_data[rel_data["username"] == user][["anime_id", "my_score"]]
   user_data = pd.Series(np.zeros(len(unique_anime)))
-  user_anime["my_score"][user_anime["my_score"] == 0.0] = 5
-  user_data[user_anime["anime_id"].apply(lambda x: anime_id_map[x])] = user_anime["my_score"] - 5
+  # user_anime["my_score"][user_anime["my_score"] == 0.0] = 5
+  user_data[user_anime["anime_id"].apply(lambda x: anime_id_map[x])] = user_anime["my_score"]
 
   all_user_data.append(user_data)
 
